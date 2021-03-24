59bdc20e3e30d1bc71d2605efddf98ae2eea7fc6
Timothy Simanhadi <44218116+Timmichi@users.noreply.github.com>
2021-03-24 13:39:42 -0700

    Add files via upload

---
+++ utils/download.py
@@ -0,0 +1,19 @@
+import requests
+import cbor
+import time
+
+from utils.response import Response
+
+
+def download(url, config, logger=None):
+    host, port = config.cache_server
+    resp = requests.get(
+        f"http://{host}:{port}/",
+        params=[("q", f"{url}"), ("u", f"{config.user_agent}")])
+    if resp:
+        return Response(cbor.loads(resp.content))
+    logger.error(f"Spacetime Response error {resp} with url {url}.")
+    return Response({
+        "error": f"Spacetime Response error {resp} with url {url}.",
+        "status": resp.status_code,
+        "url": url})
